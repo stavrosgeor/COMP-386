844cfd94d53c380779ec8a560fa60ab45f24f302
Stavros Georgiou <georgiostavros@gmail.com>
2021-11-04 19:29:36 +0200

    Movement
    adding movement to me player

--- 2D_Game/Assets/PlayerMovement.cs
+++ 2D_Game/Assets/PlayerMovement.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKeyDown("space"))
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f,rb.velocity.y);
+
+
+        if(Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0); 
+           rb.velocity = new Vector2(rb.velocity.x, 14f); 
         }
     }
 }
