8f94c61a9ab049491bc02df861261518454d7185
Stavros Georgiou <georgiostavros@gmail.com>
2022-01-10 13:32:47 +0200

    backroundMusic added , and dying audio

--- 2D_Game/Assets/Scripts/PlayerLife.cs
+++ 2D_Game/Assets/Scripts/PlayerLife.cs
@@ -8,6 +8,8 @@ public class PlayerLife : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    [SerializeField] private AudioSource deathSoundEffect;
+
 
     private void Start()
     {
@@ -25,6 +27,7 @@ public class PlayerLife : MonoBehaviour
 
     private void Die()
     {
+        deathSoundEffect.Play();  // audio when dying
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death"); //death animator  
     }
